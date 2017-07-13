50f7b433ccf8e843e86d9c3fe4cd51be198d6d54
Richard Davies <richard@monsterpuss.co.uk>
2017-07-13 15:56:53 +0100

    initial upload

---
+++ policy-assign.ps1
@@ -0,0 +1,32 @@
+param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$PolicyName = "",
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$PolicyDescription = "",
+  [Parameter(Mandatory=$True,Position=3)]
+  [string]$PolicyFile = ""
+)
+
+#Login to the Azure Resource Management Account
+Login-AzureRmAccount
+
+#Let the user choose the right subscrition
+Write-Host "---------------------------------------------------------------------"
+Write-Host "Your current subscriptions: " -ForegroundColor Yellow
+Get-AzureRMSubscription
+Write-Host "Enter the Subscription ID to deploy to: " -ForegroundColor Green
+$sub = Read-Host 
+Set-AzureRmContext -SubscriptionId $sub
+pause 
+
+$subId = (Get-AzureRmContext).Subscription.SubscriptionId
+$subName = (Get-AzureRmContext).Subscription.SubscriptionName
+
+$PolicyFile
+Write-Host "Policy is applied to the subscription: $subName" -ForegroundColor Yellow
+$policy = New-AzureRmPolicyDefinition -Name $PolicyName -Description $PolicyDescription -Policy $PolicyFile
+
+Write-Host "Adding the Policy to the subscription" -ForegroundColor Yellow
+
+#Assign the Azure Policy
+New-AzureRmPolicyAssignment -Name $PolicyName -PolicyDefinition $policy -Scope "/subscriptions/$subId"
